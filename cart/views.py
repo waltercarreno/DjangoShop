a34839137359bda0465350f8481a3b943a8e7324
waltercarreno <walter_tavernes14@hotmail.com>
2022-02-12 20:49:57 +0000

    Created footer templates and search form. Fixed css problems

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,15 @@
 from django.views import generic
 from .models import Product
-
+from django.contrib import messages
 
 class ProductListView(generic.ListView):
     template_name = 'cart/products.html'
     model = Product
     paginate_by = 3
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            object_list = self.model.objects.filter(name__icontains=query)
+        else: 
+            object_list = self.model.objects.all()
+        return object_list
